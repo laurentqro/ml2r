a8ddf3012c40d4d01f3749a48b3b131525d5b7a2
Laurent Curau <github@lqro.slmail.me>
2025-03-18 12:26:24 +0100

    Fix linting

--- db/migrate/20250317163348_change_adverse_media_checks_to_polymorphic.rb
+++ db/migrate/20250317163348_change_adverse_media_checks_to_polymorphic.rb
@@ -3,7 +3,7 @@ class ChangeAdverseMediaChecksToPolymorphic < ActiveRecord::Migration[8.0]
     remove_column :adverse_media_checks, :client_id
     add_column :adverse_media_checks, :adverse_media_checkable_id, :integer
     add_column :adverse_media_checks, :adverse_media_checkable_type, :string
-    add_index :adverse_media_checks, [:adverse_media_checkable_id, :adverse_media_checkable_type]
+    add_index :adverse_media_checks, [ :adverse_media_checkable_id, :adverse_media_checkable_type ]
   end
 
   def down
