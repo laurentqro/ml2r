42c1a09ab2485d968b1a5c9fd721eb4e23cd7574
Laurent Curau <github@lqro.slmail.me>
2025-05-09 13:35:47 +0200

    Remove basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
-  http_basic_authenticate_with name: Rails.application.credentials.basic_auth[:username], password: Rails.application.credentials.basic_auth[:password]
+  # http_basic_authenticate_with name: Rails.application.credentials.basic_auth[:username], password: Rails.application.credentials.basic_auth[:password]
 end
